197eca5efe19650ac3d89c92acbd540eda05c556
张康宁（实习） <zhangkangning@xiaohongshu.com>
2023-11-17 17:47:11 +0800

    test

---
+++ scripts.sh
@@ -0,0 +1,5 @@
+python -u DreamRec.py --data yc --timesteps 500 --lr 0.001 --beta_sche exp --w 2 --optimizer adamw --diffuser_type mlp1 --random_seed 100
+
+python -u DreamRec.py --data ks --timesteps 2000 --lr 0.00005 --beta_sche cosine --w 2 --optimizer adamw --diffuser_type mlp1 --random_seed 100
+
+python -u DreamRec.py --data zhihu --timesteps 500 --lr 0.01 --beta_sche linear --w 4 --optimizer adamw --diffuser_type mlp1 --random_seed 100 
